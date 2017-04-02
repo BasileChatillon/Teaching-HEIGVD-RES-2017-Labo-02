b89d7a0c60d2ad379477573eec9197b9bc756e82
Basile Châtillon <basile.chatillon@heig-vd.ch>
2017-04-02 15:31:42 +0200

    fix merge error

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2BasileChatillonTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2BasileChatillonTest.java
@@ -9,10 +9,7 @@ import java.util.LinkedList;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-<<<<<<< HEAD
-=======
 import org.junit.Ignore;
->>>>>>> fb-lab02-v2-tests
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
